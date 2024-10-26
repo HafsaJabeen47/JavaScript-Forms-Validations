de883e7c3f96d8866e06b58f3c49ea74d508ccdb
Hafsa <hafsajabeen47@gmail.com>
2024-10-26 16:22:26 +0500

    Get input from user

---
+++ 01-JS-Form/Form.js
@@ -0,0 +1,7 @@
+// HOt to get input value from user
+function getInput(){
+    let userInput = document.querySelector('#userInput').value;
+    let userPassword = document.querySelector('#userPassword').value;
+    alert('the user name is' + userInput);
+    alert('the user password is' + userPassword);
+};
