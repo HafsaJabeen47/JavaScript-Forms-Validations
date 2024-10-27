f336ac7ef8fdda918af87dc048d63d85047fd773
Hafsa <hafsajabeen47@gmail.com>
2024-10-27 15:28:36 +0500

    HTML and CSS Form

--- 01-JS-Form/Form.js
+++
@@ -1,7 +0,0 @@
-// HOt to get input value from user
-function getInput(){
-    let userInput = document.querySelector('#userInput').value;
-    let userPassword = document.querySelector('#userPassword').value;
-    alert('the user name is' + userInput);
-    alert('the user password is' + userPassword);
-};
