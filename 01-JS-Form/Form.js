b090a0c1b2222f5a8a58cc35eba631bac7ce914b
Hafsa <hafsajabeen47@gmail.com>
2024-10-28 21:56:27 +0500

    form

---
+++ 01-JS-Form/Form.js
@@ -0,0 +1,10 @@
+let form = document.querySelector('#form');
+let userName = document.querySelector('#username');
+let email = document.querySelector('#email');
+let phoneNo = document.querySelector('#phoneNo');
+let password = document.querySelector('#password');
+let cPassword = document.querySelector('#cPassword');
+
+form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+})
